8bd86fa859f61a285abeeac602285b7d00f13305
zhaozepin <zhaozp1@geovis.com.cn>
2024-04-15 17:26:21 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    // https://cn.vitejs.dev/config/#resolve-alias
-    alias: {
-      // 设置路径
-      '~':
-          path.resolve( './test/'),
-      // // 设置别名
-      // '@':
-      //     path.resolve(__dirname, './src'),
-    }
-    ,
-    // https://cn.vitejs.dev/config/#resolve-extensions
-    // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  }
+  // resolve: {
+  //   // https://cn.vitejs.dev/config/#resolve-alias
+  //   alias: {
+  //     // 设置路径
+  //     '~':
+  //         path.resolve( './test/'),
+  //     // // 设置别名
+  //     // '@':
+  //     //     path.resolve(__dirname, './src'),
+  //   }
+  //   ,
+  //   // https://cn.vitejs.dev/config/#resolve-extensions
+  //   // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  // }
 })
